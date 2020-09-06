51c90a5063d313b4970b7a79c72672f6d45bfff0
jundic <jundi_chen@sfu.ca>
2020-09-05 23:08:12 -0700

    Updates

---
+++ precache-manifest.ffa43e7cb4b4ed798698310e0fb4ca1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058e7bf1f5449edb8f9a87e0b8b1757e",
+    "url": "/twelvethWebAjHairFrontHeading/index.html"
+  },
+  {
+    "revision": "774acafd3db57224496e",
+    "url": "/twelvethWebAjHairFrontHeading/static/css/main.47144c23.chunk.css"
+  },
+  {
+    "revision": "c420531a23e5a5af0022",
+    "url": "/twelvethWebAjHairFrontHeading/static/js/2.c25da635.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twelvethWebAjHairFrontHeading/static/js/2.c25da635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774acafd3db57224496e",
+    "url": "/twelvethWebAjHairFrontHeading/static/js/main.421250f8.chunk.js"
+  },
+  {
+    "revision": "783756fd3921cba56d93",
+    "url": "/twelvethWebAjHairFrontHeading/static/js/runtime-main.4e2bbddb.js"
+  },
+  {
+    "revision": "0f64accd6dd2b8bcb149a187a7cc55b3",
+    "url": "/twelvethWebAjHairFrontHeading/static/media/companyLogo.0f64accd.JPG"
+  }
+]);
