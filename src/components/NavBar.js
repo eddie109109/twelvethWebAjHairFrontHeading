2c5c529cdba10fafe431c0a50084a7f6a1b7d267
jundic <jundi_chen@sfu.ca>
2020-09-05 23:16:29 -0700

    added link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,22 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <div className="navBar">
-      <a className="navBar_stylists" href="/">
+      <a
+        className="navBar_stylists"
+        href="https://eddie109109.github.io/twelvethWebAjHairFrontHeading/"
+      >
         <h3>Stylists</h3>
       </a>
-      <a className="navBar_ourWork" href="/">
+      <a
+        className="navBar_ourWork"
+        href="https://eddie109109.github.io/twelvethWebAjHairFrontHeading/"
+      >
         <h3>Our Work</h3>
       </a>
-      <a className="navBar_aboutUs" href="/">
+      <a
+        className="navBar_aboutUs"
+        href="https://eddie109109.github.io/twelvethWebAjHairFrontHeading/"
+      >
         <h3>About us</h3>
       </a>
     </div>
